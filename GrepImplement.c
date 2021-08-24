57a914ea0e3b646ba7eed3a86213a5b6b6b689c2
Mayuri <mayurimali05681@gmail.com>
2021-08-25 05:23:47 +0530

    Added function init and appended data in list

--- GrepImplement.c
+++ GrepImplement.c
@@ -2,3 +2,27 @@
 #include<conio.h>
 #include "header.h";
 
+ void init(list *head){
+
+  *head = NULL;
+  return;
+  }
+ void append_in_list(list *head, char ch) {
+
+    node* nn = (node*) malloc(sizeof(node));
+    if(nn) {
+        nn->data = ch;
+        nn->next = NULL;
+    }
+    else
+        return;
+    if(*head == NULL)
+        *head = nn;
+    else {
+        node* p = *head;
+        while(p->next)
+            p = p->next;
+        p->next = nn;
+    }
+    return;
+ }
--- main.c
+++ main.c
@@ -2,3 +2,53 @@
 #include<conio.h>
 #include "header.h";
 
+ int main(){
+    list l1,head;
+    int choice;
+    char ch, path[100];
+   // int chh =0;
+    init(&l1);
+    FILE *file;
+    up:printf("Enter file path with file name\n");
+    scanf("%s", &path);
+    file = fopen(path, "r");
+    if(file == NULL){
+        printf("Unable to open file\n");
+        goto up;
+   } else {
+        while((ch = fgetc(file)) != EOF)
+            append_in_list(&l1, ch);
+    }
+    char sear[100];
+
+    printf("***********IMPLEMENTATION OF GREP COMMAND***************\n");
+    while(1){
+    printf("\t1. Command -o: Print only the matched parts of a matching line\n");
+    printf("\t2. Command -c: This prints only a count of the lines that match a pattern\n");
+    printf("\t3. Command -n: Display the matched lines and their line numbers.\n");
+    printf("\t4. Command -w: Match whole word.\n");
+    printf("\t5. Exit \n");
+    printf("\tEnter your choice: ");
+    scanf("%d", &choice);
+    switch(choice){
+         case 1:
+
+              break;
+         case 2:
+
+              break;
+         case 3:
+
+              break;
+         case 4:
+
+              break;
+         case 5:
+              exit(0);
+         default:
+              printf("\nInvalid choice");
+        }
+    }
+
+    return 0;
+ }
